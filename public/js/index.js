d35cda5d176cdfecb1a1968b2da1b8d8fbcb4474
Omar Shariff <wuzzzzaah@yahoo.com>
2014-02-28 02:18:47 +0800

    Add events to client and server

--- public/js/index.js
+++ public/js/index.js
@@ -8,8 +8,66 @@ function init() {
 
   socket.on('connect', function () {
     sessionId = socket.socket.sessionid;
-    console.log('Connected ' + sessionId);    
+    console.log('Connected ' + sessionId);
+    socket.emit('newUser', {id: sessionId, name: $('#name').val()});    
   });
+
+  socket.on('userDisconnected', function(data) {
+    $('#' + data.id).remove();
+  });
+
+  socket.on('nameChanged', function (data) {
+    $('#' + data.id).html(data.name + ' ' + (data.id === sessionId ? '(You)' : '') + '<br />');
+  });
+
+  socket.on('incomingMessage', function (data) {
+    var message = data.message;
+    var name = data.name;
+    $('#messages').prepend('<b>' + name + '</b><br />' + message + '<hr />');
+  });
+
+  socket.on('error', function (reason) {
+    console.log('Unable to connect to server', reason);
+  });
+
+  function sendMessage() {
+    var outgoingMessage = $('#outgoingMessage').val();
+    var name = $('#name').val();
+    $.ajax({
+      url:  '/message',
+      type: 'POST',
+      dataType: 'json',
+      data: {message: outgoingMessage, name: name}
+    });
+  }
+
+  function outgoingMessageKeyDown(event) {
+    if (event.which == 13) {
+      event.preventDefault();
+      if ($('#outgoingMessage').val().trim().length <= 0) {
+        return;
+      }
+      sendMessage();
+      $('#outgoingMessage').val('');
+    }
+  }
+
+  function outgoingMessageKeyUp() {
+    var outgoingMessageValue = $('#outgoingMessage').val();
+    $('#send').attr('disabled', (outgoingMessageValue.trim()).length > 0 ? false : true);
+  }
+
+  function nameFocusOut() {
+    var name = $('#name').val();
+    socket.emit('nameChange', {id: sessionId, name: name});
+  }
+
+
+  $('#outgoingMessage').on('keydown', outgoingMessageKeyDown);
+  $('#outgoingMessage').on('keyup', outgoingMessageKeyUp);
+  $('#name').on('focusout', nameFocusOut);
+  $('#send').on('click', sendMessage);
+
 }
 
 $(document).on('ready', init);
