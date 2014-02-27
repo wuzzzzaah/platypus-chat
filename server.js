4f805a484c04f6e4af691d441c47d4941c52e7ce
Omar Shariff <wuzzzzaah@yahoo.com>
2014-02-28 00:34:48 +0800

    Setup http server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello Platypuses!");
+
+var express = require("express")
+  , app = express()
+  , http = require("http").createServer(app);
+
+app.set("ipaddress", "127.0.0.1");
+
+app.set("port", 8080);
+
+app.get("/", function(request, response) {
+  response.send("Server is online");
+
+});
+
+http.listen(app.get("port"), app.get("ipaddress"), function() {
+  console.log("Server is online. Try http:// " + app.get("ipaddress") + ":" + app.get("port"));
+});
